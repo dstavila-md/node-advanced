8db9a41e050a05ae62623b2e65a39d67430613f7
XenderMd <denis.stavilamd@gmail.com>
2025-11-22 18:01:41 +0200

    restoring the blog routes handler

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,23 +14,8 @@ module.exports = (app) => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient({ url: redisUrl });
-    const util = require('util');
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis related to this user?
-    const cachedBlogs = await client.get(req.user.id);
-    // If yes, then respond to the request right away and return
-    if (cachedBlogs) {
-      return res.send(cachedBlogs);
-    }
-    // If no, we need to respond to request and update our cache to store the data for future requests
-
     const blogs = await Blog.find({ _user: req.user.id });
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
