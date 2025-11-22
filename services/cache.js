b5af175703d6b5b9d795345b702a22a8028b8b53
XenderMd <denis.stavilamd@gmail.com>
2025-11-22 18:12:25 +0200

    key creation

--- services/cache.js
+++ services/cache.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
-  console.log('Im about to run a query');
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+  console.log(key);
   return exec.apply(this, arguments);
 };
