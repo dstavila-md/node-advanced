b26d0a2d6fd47e6cc0d89fd957abfb7f7c67c4a7
XenderMd <denis.stavilamd@gmail.com>
2025-11-23 17:04:29 +0200

    afterEach browser termination

--- tests/header.test.js
+++ tests/header.test.js
@@ -8,9 +8,11 @@ beforeEach(async () => {
   await page.goto('http://localhost:3000');
 });
 
+afterEach(async () => {
+  await browser.close();
+});
+
 test('We can launch a browser', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
   expect(text).toBe('Blogster');
-
-  await browser.close();
 });
