93460c657b503ce170e3be4af61d52145c729c2b
XenderMd <denis.stavilamd@gmail.com>
2025-11-23 16:48:21 +0200

    chromium navigation

--- tests/header.test.js
+++ tests/header.test.js
@@ -8,4 +8,5 @@ test('adds two numbers', () => {
 test('We can launch a browser', async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  await page.goto('http://localhost:3000');
 });
