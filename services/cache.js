1d8bfd12dc84b59430b81bdb272b9442911df181
XenderMd <denis.stavilamd@gmail.com>
2025-11-23 14:36:57 +0200

    toggleable cache

--- services/cache.js
+++ services/cache.js
@@ -4,10 +4,20 @@ const util = require('util');
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
-
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function () {
+  this.useCache = true;
+  return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+  // if not using cache, run the original exec function
+  this.useCache = this.useCache || false;
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name,
