19009db73508a4f71f8b6887932a448de28db3d6
XenderMd <denis.stavilamd@gmail.com>
2025-11-23 17:03:00 +0200

    dry tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,14 @@
 const puppeteer = require('puppeteer');
 
-test('adds two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toBe(3);
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000');
 });
 
 test('We can launch a browser', async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
-
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
   expect(text).toBe('Blogster');
 
