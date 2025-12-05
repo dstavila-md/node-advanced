7286fee17b2092421810360b85c1c22dc2c54d46
XenderMd <denis.stavilamd@gmail.com>
2025-12-05 22:32:53 +0200

    function lookup priority + encapsulating the login functionality

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,40 +1,29 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require("./helpers/page");
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
-  await page.goto('http://localhost:3000');
+  page = await Page.build();
+  await page.goto("http://localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
-test('The header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
-  expect(text).toBe('Blogster');
+test("The header has the correct text", async () => {
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  expect(text).toBe("Blogster");
 });
 
-test('Clicking login starts OAuth flow', async () => {
-  await page.click('.right a');
+test("Clicking login starts OAuth flow", async () => {
+  await page.click(".right a");
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test('When signed in, shows logout button', async () => {
-  const user = await userFactory();
-  const { session, signature } = sessionFactory(user);
-
-  await page.setCookie({ name: 'session', value: session });
-  await page.setCookie({ name: 'session.sig', value: signature });
-  await page.goto('http://localhost:3000');
-
-  await page.waitFor('a[href="/auth/logout"]');
-
+test("When signed in, shows logout button", async () => {
+  await page.login();
   const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
-  expect(text).toEqual('Logout');
+  expect(text).toEqual("Logout");
 });
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,23 +1,35 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
 
 class CustomPage {
-    static async build(){
-        const browser = await puppeteer.launch({
-            headless: false,
-        });
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
 
-        const page = await browser.newPage();
-        const customPage = new CustomPage(page);
-        return new Proxy(customPage, {
-            get: function(target, property){
-                return customPage[property] || page[property] || browser[property];
-            }
-        });
-    }
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return customPage[property] || browser[property] || page[property];
+      },
+    });
+  }
 
-    constructor(page){
-        this.page = page;
-    }
+  constructor(page) {
+    this.page = page;
+  }
+
+  async login() {
+    const user = await userFactory();
+    const { session, signature } = sessionFactory(user);
+
+    await this.page.setCookie({ name: "session", value: session });
+    await this.page.setCookie({ name: "session.sig", value: signature });
+    await this.page.goto("http://localhost:3000");
+    await this.page.waitFor('a[href="/auth/logout"]');
+  }
 }
 
 module.exports = CustomPage;
