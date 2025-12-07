1f8e655aa8a82d24eca85de5c80ca918ab121b68
XenderMd <denis.stavilamd@gmail.com>
2025-12-08 00:02:17 +0200

    testing prohibited actions via direct API requests from the chromium browser instance

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -54,3 +54,20 @@ describe("When logged in", async () => {
     });
   });
 });
+
+describe("When not logged in", async () => {
+  test(" User cannot create blog posts ", async () => {
+    const result = await page.evaluate(() => {
+      return fetch("/api/blogs", {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: "My Title", content: "My Content" }),
+      }).then((res) => res.json());
+    });
+
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+});
