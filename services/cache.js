d9d71fb4c5a94bdfaedb11b3303690a142907fad
XenderMd <denis.stavilamd@gmail.com>
2025-12-14 19:59:46 +0200

    script + server config for CI

--- services/cache.js
+++ services/cache.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const keys = require("../config/keys");
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
-  this.hashKey = JSON.stringify(options.key || '');
+  this.hashKey = JSON.stringify(options.key || "");
   return this;
 };
 
@@ -38,7 +38,7 @@ mongoose.Query.prototype.exec = async function () {
 
   // Otherwise, issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
-  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10); // expire in 10 seconds
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10); // expire in 10 seconds
   return result;
 };
 
