977a9103c1e0edfe63209d87209adaa5a50e47ae
XenderMd <denis.stavilamd@gmail.com>
2025-12-18 23:19:01 +0200

    configuring the aws sdk

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,3 +1,14 @@
+import AWS from "aws-sdk";
+import keys from "../config/keys.js";
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+  },
+  region: "us-east-1",
+});
+
 module.exports = (app) => {
   app.get("/api/upload", (req, res) => {});
 };
